b2ef6f911b7b980d4ec4a6318699f7df311c9a35
RafoDev <rafael.david@ucsp.edu.pe>
2023-11-06 05:24:21 -0500

    feat(backend): search endpoint updated.

---
+++ backend/backend/utils.py
@@ -0,0 +1,45 @@
+from pymongo import MongoClient
+from porterStemmer import PorterStemmer
+
+client = MongoClient('localhost', 27017)
+db = client['search_engine_db']
+
+collection_ii = db['inverted_index']
+collection_pr = db['page_rank']
+
+def get_docs_by_word(word):
+    document = collection_ii.find_one({"word": word})
+    if document:
+        return document['postings']
+    else:
+        return None
+
+def get_rank_by_doc(filename):
+    document = collection_pr.find_one({"filename": filename})
+    if document:
+        return document['rank']
+    else:
+        return None
+
+
+porter=PorterStemmer()
+
+def search_query(value):
+
+	words = []
+
+	for word in value:
+		words.append(word)
+		word =	porter.stem(word, 0, len(word)-1)
+
+	if len(value):
+		result = []
+		i = 0
+		for word in value:
+			word_is_in_ii = collection_ii.find_one({"word": word})
+			if word_is_in_ii:
+				result.append([get_docs_by_word(word), words[i]])
+				i += 1
+		return result
+
+	return []			
--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,25 +1,48 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from backend.models import InvertedIndex
+from django.views.decorators.csrf import csrf_exempt
+
 from pymongo import MongoClient
+from .utils import search_query, get_rank_by_doc
+
 
 @api_view(['GET'])
 def hello(request):
     return JsonResponse({'message': 'Hola'})
+
 
 @api_view(["POST"])
 def search(request):
     # Conectar con la instancia de MongoDB directamente
     client = MongoClient('localhost', 27017)
     db = client['search_engine_db']
-    
-    # Obtener palabras de la solicitud
-    words = request.data.get('words', [])
-    
-    # Buscar documentos para cada palabra
-    documents = {}
-    for word in words:
-        cursor = db.inverted_index.find({"word": word}, {"_id": 0, "postings": 1})
-        documents[word] = list(cursor)
 
-    return JsonResponse(documents)
+    page_rank = {}
+
+    words = request.data.get("words", "").lower().split()
+    response = search_query(words)
+    pagelist = []
+
+    if not len(response):
+        return Response([])
+
+    else:
+        for lista in response:
+            for page in lista[0]:
+                if page in page_rank:
+                    page_rank[page][1] += ' '+lista[1]
+                else:
+                    page_rank[page] = [get_rank_by_doc(page), lista[1]]
+
+    page_rank = {k: v for k, v in sorted(
+        page_rank.items(), reverse=True, key=lambda item: item[1][0])}
+
+    if len(page_rank) <= 10:
+        pagelist = [[key, page_rank[key]] for key in page_rank]
+    else:
+        temp_dic = dict(list(page_rank.items())[:10])
+        pagelist = [[key, temp_dic[key]] for key in temp_dic]
+
+    return Response(pagelist)
