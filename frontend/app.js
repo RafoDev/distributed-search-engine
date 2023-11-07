6bfb7d887725b565a0d4b70e1b9b3e12b8b96d04
RafoDev <rafael.david@ucsp.edu.pe>
2023-11-07 14:16:53 -0500

    feat(frontend): search if click and new styles.

--- frontend/app.js
+++ frontend/app.js
@@ -1,38 +1,48 @@
-const displayResults = (results) => {
-    var resultsContainer = document.getElementById("results");
-  
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.getElementById("searchInput");
+  const button = document.getElementById("button");
+
+  const displayResults = (results) => {
+    const resultsContainer = document.getElementById("results");
+
     resultsContainer.innerHTML = "";
-  
-    results.forEach(function (item) {
-      var card = document.createElement("div");
+
+    results.forEach(item => {
+      const card = document.createElement("div");
       card.className = "card";
       card.innerHTML = "<h3>" + item[0] + "</h3>" + "<p>" + item[1] + "</p>";
       resultsContainer.appendChild(card);
     });
   };
 
-const search = () => {
-  var words = document.getElementById("searchInput").value;
+  const search = () => {
+    const words = input.value;
+    const dataToSend = {
+      words: words,
+    };
 
-  const dataToSend = {
-    words: words,
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dataToSend),
+    };
+
+    fetch("http://204.236.220.115:8000/search", requestOptions)
+      .then((response) => response.json())
+      .then((data) => displayResults(data))
+      .catch((error) => console.error("Error:", error));
   };
-
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(dataToSend),
-  };
-
-  fetch("http://204.236.220.115:8000/search", requestOptions)
-    .then((response) => response.json())
-    .then((data) => displayResults(data))
-    .catch((error) => console.error("Error:", error));
-};
-
-var button = document.getElementById("button");
-window.addEventListener('click', () =>{
+  
+  button.addEventListener("click", () => {
     search();
-})
+  });
+
+  input.addEventListener("keypress", event => {
+    if(event.key === 'Enter'){
+      button.click();
+    }
+  })
+
+});
