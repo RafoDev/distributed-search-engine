690279dc506b97c1240d59ac5b43d1baee12d87e
RafoDev <rafael.david@ucsp.edu.pe>
2023-11-07 16:53:19 -0500

    feat(corpus-download): new way to filter openAccess papers.

--- corpus/corpus-generator.py
+++ corpus/corpus-generator.py
@@ -5,7 +5,6 @@ import json
 import PyPDF2
 from downloader import download_papers_pdf
 import boto3
-from botocore.exceptions import NoCredentialsError
 from io import BytesIO
 
 from config import bucket_name
@@ -25,46 +24,79 @@ class Paper:
         self.referenced_papers = []
         self.error = False
 
-        self.get_references()
-
-        if (not self.has_error()):
-            papers.append(pid)
+        self.error = self.download_paper_pdf(self.pid)
+        if not self.has_error():
+            self.get_references()
+            if not self.has_error():
+                papers.append(pid)
 
     def add_referenced_paper(self, paper):
         self.referenced_papers.append(paper)
+
+    def download_paper_pdf(self, pid):
+        s3_client = boto3.client('s3')
+        params = {
+            'fields': 'title,authors,abstract,paperId,isOpenAccess,openAccessPdf'
+        }
+        headers = {
+            'user-agent': 'requests/2.0.0',
+        }
+
+        url = f'https://api.semanticscholar.org/graph/v1/paper/{pid}'
+
+        response = requests.get(url, params=params, headers=headers)
+
+        if (response.status_code == 200):
+            paper = response.json()
+
+            if not paper['isOpenAccess']:
+                return True
+
+            if paper['openAccessPdf'] is None:
+                return True
+
+            paper_url = paper['openAccessPdf']['url']
+            path = 'corpus/pdf/'+pid+'.pdf'
+
+            headers = {
+                'user-agent': 'requests/2.0.0',
+            }
+
+            response = requests.get(
+                paper_url, headers=headers, stream=True)
+
+            if response.status_code == 200:
+                pdf_data = BytesIO()
+                if response.headers['content-type'] != 'application/pdf':
+                    return True
+                for chunk in response.iter_content(chunk_size=8192):
+                    pdf_data.write(chunk)
+                pdf_data.seek(0)
+                s3_client.upload_fileobj(pdf_data, bucket_name, path)
+                pdf_data.close()
+            else:
+                return True
+
+        else:
+            return True
+        return False
 
     def get_references(self):
         url = f"https://api.semanticscholar.org/graph/v1/paper/{self.pid}/references"
         response = requests.get(url)
         if response.status_code == 200:
             results = response.json()
-            tmp_references = results.get("data",[])
+            tmp_references = results.get("data", [])
 
-            tmp_pids = [ref["citedPaper"]["paperId"] for ref in tmp_references if "citedPaper" in ref]
+            tmp_pids = [ref["citedPaper"]["paperId"]
+                        for ref in tmp_references if "citedPaper" in ref]
 
-            if tmp_pids:
-
-                r = requests.post(
-                    'https://api.semanticscholar.org/graph/v1/paper/batch',
-                    params={'fields': 'isOpenAccess'},
-                    json={
-                        "ids": tmp_pids}
-                )
-                if (r.status_code == 200):
-                    results = r.json()
-                    for ref in results:
-                        if isinstance(ref, dict) and ref.get("isOpenAccess") is True:
-                            self.references.append(ref["paperId"])
-
-                else:
-                    self.error = True
-                    return
-
-            n_references = len(self.references)
-            n_papers = max_references if n_references > max_references else n_references
-
-            self.references = self.references[:n_papers]
-
+            for pid in tmp_pids:
+                if len(self.references) == max_references:
+                    break
+                error = self.download_paper_pdf(pid)
+                if not error:
+                    self.references.append(pid)
         else:
             self.error = True
 
@@ -100,44 +132,46 @@ def tree_to_json(paper):
     tree_dict = paper.to_dict()
     traverse_tree_util(paper, tree_dict)
 
-    json_data_bytes =  json.dumps(tree_dict, indent=4).encode('utf-8')
-   
+    json_data_bytes = json.dumps(tree_dict, indent=4).encode('utf-8')
+
     s3_client = boto3.client('s3')
 
     json_buffer = BytesIO(json_data_bytes)
 
-    s3_client.upload_fileobj(json_buffer,bucket_name, 'data/corpus.json')
+    s3_client.upload_fileobj(json_buffer, bucket_name, 'data/corpus.json')
 
     json_buffer.close()
 
 
-
-def download_papers():
+def papers_to_txt():
     global papers
     s3_client = boto3.client('s3')
 
-    download_papers_pdf(papers)
+    # download_papers_pdf(papers)
 
     for paper in papers:
         content = ""
-        
-        pdf_object = s3_client.get_object(Bucket=bucket_name,Key=f"corpus/pdf/{paper}.pdf")
+
+        pdf_object = s3_client.get_object(
+            Bucket=bucket_name, Key=f"corpus/pdf/{paper}.pdf")
         pdf_data = BytesIO(pdf_object['Body'].read())
 
         pdf_reader = PyPDF2.PdfReader(pdf_data)
-        
+
         for page in pdf_reader.pages:
             page_text = page.extract_text()
             content += page_text
 
         text_data = BytesIO(content.encode('utf-8'))
-        s3_client.upload_fileobj(text_data, bucket_name, f'corpus/txt/{paper}.txt'.format(paper))
+        s3_client.upload_fileobj(
+            text_data, bucket_name, f'corpus/txt/{paper}.txt'.format(paper))
 
         pdf_data.close()
         text_data.close()
+
 
 if __name__ == "__main__":
     root = Paper("649def34f8be52c8b66281af98ae884c09aef38b")
     traverse_references(root, 0)
     tree_to_json(root)
-    download_papers()
+    papers_to_txt()
